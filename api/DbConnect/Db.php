589678a780b63f124169cdc2069332aad94d553a
Hieu Tn <tnhieu123@gmail.com>
2018-12-11 00:59:11 +0700

    cap nhat api 1q

--- api/DbConnect/Db.php
+++ api/DbConnect/Db.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = mysqli_connect("localhost", "appgt", "appgt", "appgt");
+$mysqli = mysqli_connect("localhost", "root", "", "appgt");
 $mysqli->set_charset("utf8");
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
